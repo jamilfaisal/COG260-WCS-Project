ce60690cc579b9777650d43b3e8ee138681edfe5
Faisal Jamil <faisal.jamil@mail.utoronto.ca>
2022-11-19 14:16:28 -0500

    - Refactor main for loop into more methods - Add docstring for all methods - Remove commented out code from implementation for previous hypothesis - Delete figures folder from previous hypothesis - Rename wcs_demo_notebook.py to COG260_project_faisal_zelong.py

---
+++ Archive Code.py
@@ -0,0 +1,20 @@
+# Color chips, Letter and number, 330 color chips
+# munsellInfo = readChipData('./WCS_data_core/chip.txt')
+
+# fociData = readFociData('./WCS_data_core/foci-exp.txt')
+
+# plt.subplot(5, 10, 4)
+# # generate chip
+# encoded_terms_male = map_array_to(curr_male_chip, generate_random_values(curr_male_chip))
+# plotValues(encoded_terms_male, language_index, "male")
+# encoded_terms_female = map_array_to(curr_female_chip, generate_random_values(curr_female_chip))
+# plotValues(encoded_terms_female, language_index, "female")
+# # count difference
+# diff_count = 0
+# for l in range(1, len(curr_male_chip)):
+#
+#     if curr_male_chip[l] != curr_female_chip[l]:
+#         diff_count += 1
+# # print(diff_count)
+# diff_perc = diff_count / 330
+# diff_perc_dict[language_index] = diff_perc
